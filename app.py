1954e89a0ba593ad14526440badb6965eb62b9fb
AishwaryaMS <msaishwarya37@gmail.com>
2026-05-11 13:04:14 +0530

    Set up Flask app with a home route

--- app.py
+++ app.py
@@ -1,1 +1,12 @@
+# app.py
 
+from flask import Flask
+
+app = Flask(_name_)
+
+@app.route("/")
+def home():
+    return "Hello! Docker is running this Python app successfully "
+
+if _name_ == "_main_":
+    app.run(host="0.0.0.0", port=5000)
